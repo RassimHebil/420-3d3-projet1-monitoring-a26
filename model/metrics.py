b55baf6b969731f6bba2bcd3c6c7791b443c577e
Hebil <e6292216@cmaisonneuve.qc.ca>
2026-09-04 12:27:43 -0400

    ajout des metriques

--- model/metrics.py
+++ model/metrics.py
@@ -1,2 +1,22 @@
 import psutil
 from model.sujet import sujet
+class MetriqueSysteme(sujet):
+    def __init__(self):
+        super().__init__()
+        self._cpu = None
+        self._ram = None
+        self._disque = None
+    def actualiser_metriques(self) -> None:
+        self._cpu = psutil.cpu_percent(interval=None)
+        self._ram = psutil.virtual_memory().percent
+        self._disque = psutil.disk_usage('/').percent
+        self.notifier()
+
+    def get_donnees(self) -> dict:
+        donnee = {
+            "cpu": self._cpu,
+            "ram": self._ram,
+            "disque": self._disque
+        }
+        return donnee
+
